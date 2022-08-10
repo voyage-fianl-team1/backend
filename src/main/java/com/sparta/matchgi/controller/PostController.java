3aa60d09e9bc15f5927fc0a1fca8b399a237e308
PARKYUNJU <pyunj6767@gmail.com>
2022-08-10 14:53:22 +0900

    파일,json 한꺼번에 업로드

--- src/main/java/com/sparta/matchgi/controller/PostController.java
+++ src/main/java/com/sparta/matchgi/controller/PostController.java
@@ -7,6 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,14 +19,14 @@ public class PostController {
 
 
     @PostMapping("/api/posts")
-    public ResponseEntity<?> createPost(@RequestBody CreatePostRequestDto createPostRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
-        return postService.createPost(createPostRequestDto,userDetails);
+    public ResponseEntity<?> createPost(@RequestPart CreatePostRequestDto createPostRequestDto, @RequestPart MultipartFile file, @AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
+        return postService.createPost(createPostRequestDto,file,userDetails);
     }
 
-    @PutMapping("api/posts/{postId}")
-    public ResponseEntity<?> editPost(@PathVariable Long postId, @RequestBody CreatePostRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
-        return postService.editPost(postId,requestDto,userDetails);
-    }
+//    @PutMapping("api/posts/{postId}")
+//    public ResponseEntity<?> editPost(@PathVariable Long postId, @RequestBody CreatePostRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
+//        return postService.editPost(postId,requestDto,userDetails);
+//    }
 
 
 
