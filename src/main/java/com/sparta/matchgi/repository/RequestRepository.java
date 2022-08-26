5a3e3e664976c7a71c77017a021232b05c1b8282
PARKYUNJU <pyunj6767@gmail.com>
2022-08-26 17:38:35 +0900

    지도 성능 개선 중

--- src/main/java/com/sparta/matchgi/repository/RequestRepository.java
+++ src/main/java/com/sparta/matchgi/repository/RequestRepository.java
@@ -24,6 +24,7 @@ public interface RequestRepository extends JpaRepository<Request,Long> {
 
     List<Request> findAllByUser(User user);
 
+<<<<<<< HEAD
 
 
 //    @Query("SELECT new com.sparta.matchgi.dto.GetScoresResponseDto(p.matchDeadline ,p.subject,r.requestStatus) " +
@@ -44,4 +45,6 @@ public interface RequestRepository extends JpaRepository<Request,Long> {
     void deleteAllByPost(Post post);
 
 
+=======
+>>>>>>> parent of 2a7569e (유저의 경기결과를 보여주는 api 구현)
 }
